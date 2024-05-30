d156a5f87223493f2628263a843d5ecffe175947
Timur <toombez.timurtokaev@gmail.com>
2024-05-30 09:15:34 +0300

    Add Header to Layout

--- apps/frontend/src/Layout.jsx
+++ apps/frontend/src/Layout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
 
 function Layout() {
     return (
         <div>
+            <Header />
             <Outlet />
         </div>
     )
